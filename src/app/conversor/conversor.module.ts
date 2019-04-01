aaaaa9d68b3ca9c1d6acc5d8c050a49a35e510a9
Marcos Buganeme <marcos.after@gmail.com>
2019-04-01 19:18:18 -0300

    module principal

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ConversorComponent } from './components';
+import { MoedaService } from './services';
+
 @NgModule({
-  declarations: [],
+  declarations: [
+      ConversorComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ConversorComponent
+  ],
+  providers: [
+    MoedaService
   ]
 })
 export class ConversorModule { }

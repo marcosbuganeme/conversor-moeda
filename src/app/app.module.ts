1ab87619e1d1cc99e5192516abf35c78280c2187
Marcos Buganeme <marcos.after@gmail.com>
2019-04-01 19:17:43 -0300

    registrando modulos
    FormsModule
    HttpClient
    ConversorModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ConversorModule } from './conversor/conversor.module'
+import { ConversorModule } from './conversor'
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { ConversorModule } from './conversor/conversor.module'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
     ConversorModule
   ],
   providers: [],
